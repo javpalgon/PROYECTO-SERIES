52307691d5c3c80127ec0f8c2d0dc70e86dc6735
Javier_Pallares_Gonzalez <javpalgon@alum.us.es>
2022-10-09 19:08:11 +0200

    TERMINADO

--- src/proyecto.py
+++ src/proyecto.py
@@ -1,14 +1,25 @@
 import csv
 from collections import namedtuple
+from datetime import date, datetime
 from unittest import result
 
-tvshows=namedtuple('TvShows', 'ID, Title, Year, Age, IMDb, Rotten Tomatoes, Netflix, Hulu, Prime Video, Disney Plus,Type')
+tvshows=namedtuple('TvShows', 'ID, Title, Age, rating, Netflix, Hulu, Prime_Video, Disney_Plus, Release_Date')
 def lee_datos(series):
     result = []
     with open (series, encoding= 'utf-8') as f:
-        lector = csv.reader(f)
+        lector = csv.reader(f, delimiter=";")
         next(lector)
-    for ID, Title, Year, Age, IMDb, Rotten_Tomatoes, Netflix, Hulu, Prime_Video, Disney_Plus, Type in lector:
-        tupla_con_nombre = tvshows(int(ID), str(Title), int(Year), int(Age), float(IMDb), int(Rotten_Tomatoes), int(Netflix), int(Hulu), int(Prime_Video), int(Disney_Plus), int(Type))
-        result.append(tupla_con_nombre)
+        for lista in lector:
+            ID = int(lista[0])
+            Title= str(lista[1])
+            Age = lista[2]
+            rating=float(lista[3])
+            Netflix=bool(lista[4])
+            Hulu=bool(lista[5])
+            Prime_Video=bool(lista[6])
+            Disney_Plus=bool(lista[7])
+            Release_Date= datetime.strptime(lista[8], "%d/%m/%Y").date()
+
+            tupla_con_nombre = tvshows(ID, Title, Age, rating, Netflix, Hulu, Prime_Video, Disney_Plus, Release_Date)
+            result.append(tupla_con_nombre)
     return result
--- src/test_proyecto.py
+++ src/test_proyecto.py
@@ -1,10 +1,10 @@
 from proyecto import lee_datos
 
 def main():
-    tv_series = lee_datos("./data/tv_shows.csv")
+    tv_series = lee_datos("./data/TV_SHOWS_0.csv")
 
-    print(len(tv_series))
-    print(tv_series)
+    print(tv_series[0], tv_series[1], tv_series[2]) 
+    print(tv_series[-1], tv_series[-2], tv_series[-3])
 
 if __name__ == "__main__":
     main()
