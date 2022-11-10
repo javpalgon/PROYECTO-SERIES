a90324fef91599b2540d6d4b8556065e7789d671
Javier_Pallares_Gonzalez <javpalgon@alum.us.es>
2022-11-10 19:18:52 +0100

    fre

--- src/test_proyecto.py
+++ src/test_proyecto.py
@@ -1,5 +1,9 @@
 from proyecto import lee_datos
 
+def test_filtra_por_edad(proyecto):
+    edad_series = filtra
+
+
 def main():
     tv_series = lee_datos("./data/TV_SHOWS_0.csv")
 
@@ -10,6 +14,7 @@ def main():
     print(tv_series[-1]) 
     print(tv_series[-2])
     print(tv_series[-3])
+
 
 if __name__ == "__main__":
     main()
