66f5cd20d55fe582f9a204a8f44b8a09c0d5592b
Javier_Pallares_Gonzalez <javpalgon@alum.us.es>
2022-11-12 16:55:07 +0100

    COMMIT

--- src/proyecto.py
+++ src/proyecto.py
@@ -18,7 +18,7 @@ def lee_datos(series):
             Hulu=bool(lista[5])
             Prime_Video=bool(lista[6])
             Disney_Plus=bool(lista[7])
-            Release_Date= lista[8]
+            Release_Date=datetime.strptime(lista[8],"%d/%m/%Y")
 
             tupla_con_nombre = tvshows(ID, Title, Age, rating, Netflix, Hulu, Prime_Video, Disney_Plus, Release_Date)
             result.append(tupla_con_nombre)
@@ -47,9 +47,18 @@ def valoraciones(tupla_con_nombre):
 
 def max_valoracion_series_familiares(tupla_con_nombre):
     result=[]
-    
     for i in filtra_por_edad(tupla_con_nombre):
         fin= (i.Title, i.Age, i.rating)
         if i.rating == max(valoraciones(tupla_con_nombre)):
             result.append(fin)
+    return result
+
+def agrupar_segun_fecha_lanzamiento(tupla_con_nombre):
+    result = dict()
+    for i in tupla_con_nombre:
+        clave = i.Release_Date.year
+        if clave in result:
+            result[clave] += i
+        else:
+            result[clave] = i
     return result
--- src/test_proyecto.py
+++ src/test_proyecto.py
@@ -10,10 +10,15 @@ def test_valoracion_media_netflix(proyecto):
 def test_max_valoracion_series_familiares(proyecto):
     print("Te recomendamos esta serie para ver junto a los mas peques de casa!!:", max_valoracion_series_familiares(proyecto))
 
+def test_agrupar_segun_fecha_lanzamiento(proyecto):
+    numero_lanz= agrupar_segun_fecha_lanzamiento(proyecto)
+    for i in numero_lanz.keys():
+        print("\t{}: {}".format(i, numero_lanz[i]))
+
 def main():
     tv_series = lee_datos("./data/TV_SHOWS_0.csv")
 
-    #print(tv_series[0]) 
+    #print(tv_series) 
     #print(tv_series[1])
     #print(tv_series[2]) 
     
@@ -22,7 +27,8 @@ def main():
     #print(tv_series[-3])
     #test_filtra_por_edad(tv_series)
     #test_valoracion_media_netflix(tv_series)
-    test_max_valoracion_series_familiares(tv_series)
+    #test_max_valoracion_series_familiares(tv_series)
+    test_agrupar_segun_fecha_lanzamiento(tv_series)
 
 if __name__ == "__main__":
     main()
