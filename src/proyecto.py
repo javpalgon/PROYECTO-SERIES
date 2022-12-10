d282fa346b62fc1c4ecf2d98e12669b04bfa2f18
Javier_Pallares_Gonzalez <javpalgon@alum.us.es>
2022-12-10 18:44:45 +0100

    10/12

--- src/proyecto.py
+++ src/proyecto.py
@@ -1,16 +1,16 @@
 import csv
-from collections import namedtuple
+from collections import namedtuple, defaultdict, Counter
 from datetime import date, datetime
 from unittest import result
 
-tvshows=namedtuple('TvShows', 'ID, Title, Age, rating, Netflix, Hulu, Prime_Video, Disney_Plus, Release_Date')
+tvshows=namedtuple('TvShows', 'id, Title, Age, rating, Netflix, Hulu, Prime_Video, Disney_Plus, Release_Date')
 def lee_datos(series):
     result = []
     with open (series, encoding= 'utf-8') as f:
         lector = csv.reader(f, delimiter=";")
         next(lector)
         for lista in lector:
-            ID = int(lista[0])
+            id = int(lista[0])
             Title= str(lista[1])
             Age = str(lista[2])
             rating=float(lista[3])
@@ -20,7 +20,7 @@ def lee_datos(series):
             Disney_Plus=bool(lista[7])
             Release_Date=datetime.strptime(lista[8],"%d/%m/%Y")
 
-            tupla_con_nombre = tvshows(ID, Title, Age, rating, Netflix, Hulu, Prime_Video, Disney_Plus, Release_Date)
+            tupla_con_nombre = tvshows(id, Title, Age, rating, Netflix, Hulu, Prime_Video, Disney_Plus, Release_Date)
             result.append(tupla_con_nombre)
     return result
 
@@ -62,3 +62,42 @@ def agrupar_segun_fecha_lanzamiento(series):
         else:
             result[clave] = i
     return result
+
+def total_series_por_anyo(series):
+    series_por_anyo = dict()
+    for s in series:
+        if s.Release_Date.year in series_por_anyo:
+            series_por_anyo[s.Release_Date.year] += 1
+        else:
+            series_por_anyo[s.Release_Date.year] =1
+    return series_por_anyo.items()
+
+def maximo_minimo_shows_por_anyo(series):
+    tspa = total_series_por_anyo(series)
+    maximo = max(tspa, key= lambda x: x[1])
+    minimo = min(tspa, key= lambda x: x[1])
+    return  maximo, minimo
+
+def valoraciones_shows_por_edad(series, edad):
+    result = []
+    for s in series:
+        titulo_valoracion = s.Title, s.rating
+        if s.Age == edad:
+            result.append(titulo_valoracion)
+    return result
+
+def mejor_serie_por_edad(series):
+    dicc = dict()
+    for s in series:
+        if s.Age not in dicc:
+            for s in series:
+                dicc.setdefault(s.Age, max(valoraciones_shows_por_edad(series, s.Age), key= lambda x: x[1]))
+    return dicc
+
+def mejores_series_por_edad(series, n=3):
+    diccionario = dict()
+    for s in series:
+        if s.Age not in diccionario:
+            diccionario.setdefault(s.Age, sorted(valoraciones_shows_por_edad(series, s.Age), key= lambda x:x[1], reverse = True)[:n])
+    return diccionario
+        
--- src/test_proyecto.py
+++ src/test_proyecto.py
@@ -1,34 +1,65 @@
 from proyecto import *
-def test_series_para_todos(lee_datos):
-    print("Series para todos!: ",series_para_todos(lee_datos), end=' ')
 
-def test_valoracion_media_netflix(series_netflix):
-    calculo_media = valoracion_media_netflix(series_netflix)
+def test_lee_datos(datos):
+    print(datos[0]) 
+    print(datos[1])
+    print(datos[2]) 
+    
+    print(datos[-1]) 
+    print(datos[-2])
+    print(datos[-3])
+def test_series_para_todos(datos):
+    print("Series para todos!: ",series_para_todos(datos), end=' ')
+
+def test_valoracion_media_netflix(datos):
+    calculo_media = valoracion_media_netflix(datos)
     media= (sum(calculo_media)/len(calculo_media))
     print("valoracion media de los shows de netflix:", "{0:.2f}".format(media))
 
-def test_max_valoracion_series_familiares(valoraciones):
-    print("Te recomendamos esta serie para ver junto a los mas peques de casa!!:", max_valoracion_series_familiares(valoraciones))
+def test_max_valoracion_series_familiares(datos):
+    print("Te recomendamos esta serie para ver junto a los mas peques de casa!!:", max_valoracion_series_familiares(datos))
 
-def test_agrupar_segun_fecha_lanzamiento(lee_datos):
-    numero_lanz= agrupar_segun_fecha_lanzamiento(lee_datos)
+def test_agrupar_segun_fecha_lanzamiento(datos):
+    numero_lanz= agrupar_segun_fecha_lanzamiento(datos)
     for i in numero_lanz.keys():
         print(" \t{} : {} ".format(i, numero_lanz[i]))
 
+def test_total_series_por_anyo(datos):
+    print("El número de shows que se publicaron por anyo es el siguiente:", total_series_por_anyo(datos))
+
+def test_maximo_minimo_shows_por_anyo(datos):
+    maximo, minimo = maximo_minimo_shows_por_anyo(datos)
+    print("El anyo donde más shows se publicaron fue: ", maximo, " y el mínimo: ", minimo)
+
+def test_valoraciones_shows_por_edad(datos):
+    print("La valoración más alta en un año es: ", valoraciones_shows_por_edad(datos, '18+'))
+
+def test_mejor_serie_por_edad(datos):
+    print("La mejor serie para cada edad recomendada es la siguiente:", mejor_serie_por_edad(datos))
+
+def test_mejores_series_por_edad(datos):
+    print("Top 3 series por cada edad", mejores_series_por_edad(datos, 5))
+
+
 def main():
     tv_series = lee_datos("./data/TV_SHOWS_0.csv")
-
-    #print(tv_series) 
-    #print(tv_series[1])
-    #print(tv_series[2]) 
-    
-    #print(tv_series[-1]) 
-    #print(tv_series[-2])
-    #print(tv_series[-3])
-    test_series_para_todos(tv_series)
+    #print("Entrega 1---------------------------------------------")
+    #test_lee_datos(tv_series)
+    #print("Entrega 2-----------------------------------------------------")
+    #test_series_para_todos(tv_series)
+    #print("-----------------------------------------------------")
     #test_valoracion_media_netflix(tv_series)
+    #print("-------------------------------------------------------")
     #test_max_valoracion_series_familiares(tv_series)
+    #print("------------------------------------------")
     #test_agrupar_segun_fecha_lanzamiento(tv_series)
+    #print("Entrega 3--------------------------------------------------")
+    #test_total_series_por_anyo(tv_series)
+    #print("----------------------------------------------------------------")
+    #test_maximo_minimo_shows_por_anyo(tv_series)
+    #print("------------------------------------------------------------")
+    #test_mejor_serie_por_edad(tv_series)
+    test_mejores_series_por_edad(tv_series)
 
 if __name__ == "__main__":
     main()
